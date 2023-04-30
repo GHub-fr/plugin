495ee7221f37000029da2534dcfd440caebc9184
lx78WyY0J5 <9j2k37st@gmail.com>
2023-04-30 16:34:35 +0200

    Generate world later on first run to let spawn structure build

--- src/main/java/ghub/fr/world/api/LastWorldsInit.java
+++ src/main/java/ghub/fr/world/api/LastWorldsInit.java
@@ -1,31 +1,41 @@
 package ghub.fr.world.api;
 
 import org.bukkit.WorldType;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.World.Environment;
 
+import ghub.fr.main.main;
 import ghub.fr.system.ServerBootFile;
 import ghub.fr.system.ServerBootFile.serverType;
 
 public class LastWorldsInit {
     public static void LoadWorld() {
-        serverType serverType = ServerBootFile.getServerTypeFromYML();
-        switch (serverType) {
-            case RPG:
-                return;
-            case SkyBlock:
-                return;
-            case Survie:
-            case Anarchie:
-                worldManager.Generate("world", true, Environment.NORMAL, WorldType.NORMAL, false);
-                return;
-            case OPPrison:
-                worldManager.Generate("Mine.1", false, Environment.NORMAL, WorldType.NORMAL, true);
-                // set structure
-                return;
-            case Creatif:
-                return;
-            case Hub:
-                return;
-        }
+        Plugin plugin = JavaPlugin.getPlugin(main.class);
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                serverType serverType = ServerBootFile.getServerTypeFromYML();
+                switch (serverType) {
+                    case RPG:
+                        return;
+                    case SkyBlock:
+                        return;
+                    case Survie:
+                    case Anarchie:
+                        worldManager.Generate("world", true, Environment.NORMAL, WorldType.NORMAL, false);
+                        return;
+                    case OPPrison:
+                        worldManager.Generate("Mine.1", false, Environment.NORMAL, WorldType.NORMAL, true);
+                        // set structure
+                        return;
+                    case Creatif:
+                        return;
+                    case Hub:
+                        return;
+                }
+            }
+        }.runTaskLater(plugin, 20 * 60 * 3);
     }
 }
