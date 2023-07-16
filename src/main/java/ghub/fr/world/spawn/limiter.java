7bcf6eed9e9d5a1b9d9f9b196830532f47d1baf7
lx78WyY0J5 <9j2k37st@gmail.com>
2023-07-16 21:15:25 +0200

    NPC respawn fix

--- src/main/java/ghub/fr/world/spawn/limiter.java
+++ src/main/java/ghub/fr/world/spawn/limiter.java
@@ -11,29 +11,34 @@ import org.bukkit.event.raid.RaidTriggerEvent;
 public class limiter implements Listener {
     @EventHandler
     public void onCreatureSpawn(CreatureSpawnEvent e) {
-        if (e.getLocation().getWorld().getName().startsWith("Spawn") || e.getEntity().getWorld().getName().startsWith("Mine.")
-                && !e.getSpawnReason().equals(CreatureSpawnEvent.SpawnReason.CUSTOM)) {
-            e.setCancelled(true);
+        if (e.getLocation().getWorld().getName().startsWith("Spawn")
+                || e.getLocation().getWorld().getName().startsWith("Mine.")) {
+            if (!e.getSpawnReason().equals(CreatureSpawnEvent.SpawnReason.CUSTOM)) {
+                e.setCancelled(true);
+            }
         }
     }
 
     @EventHandler
     public void onRaid(RaidTriggerEvent e) {
-        if (e.getWorld().getName().startsWith("Spawn") || e.getWorld().getName().startsWith("Mine.")) {
+        if (e.getWorld().getName().startsWith("Spawn")
+                || e.getWorld().getName().startsWith("Mine.")) {
             e.setCancelled(true);
         }
     }
 
     @EventHandler
     public void onFoodChange(FoodLevelChangeEvent e) {
-        if (e.getEntity().getWorld().getName().startsWith("Spawn") || e.getEntity().getWorld().getName().startsWith("Mine.")) {
+        if (e.getEntity().getWorld().getName().startsWith("Spawn")
+                || e.getEntity().getWorld().getName().startsWith("Mine.")) {
             e.setCancelled(true);
         }
     }
 
     @EventHandler
     public void onDamage(EntityDamageEvent e) {
-        if (e.getEntity().getWorld().getName().startsWith("Spawn")) {
+        if (e.getEntity().getWorld().getName().startsWith("Spawn")
+                || e.getEntity().getWorld().getName().startsWith("Mine")) {
             if (e.getEntityType().equals(EntityType.PLAYER)) {
                 e.setCancelled(true);
             }
