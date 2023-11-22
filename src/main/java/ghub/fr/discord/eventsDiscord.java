d670eb171d35cc6d9e6e05ea1e5197c748370ad8
lx78WyY0J5 <9j2k37st@gmail.com>
2023-11-22 17:01:47 +0100

    removed trailing space

--- src/main/java/ghub/fr/discord/eventsDiscord.java
+++ src/main/java/ghub/fr/discord/eventsDiscord.java
@@ -41,27 +41,27 @@ public class eventsDiscord {
         TextComponent PlayerComponent = new TextComponent();
         PlayerComponent.setText("[ §r §6" + messageAuthor.getDisplayName() + " §r]");
         String hover = "";
-        hover = hover.concat(" \n ");
+        hover = hover.concat("\n");
         hover = hover.concat("§6§lUUID§r: " + messageAuthor.getIdAsString());
-        hover = hover.concat(" \n ");
+        hover = hover.concat("\n");
         hover = hover.concat("§6§lPseudo: " + messageAuthor.getName());
-        hover = hover.concat(" \n ");
+        hover = hover.concat("\n");
         hover = hover.concat("§6§lTAG§r: " + messageAuthor.getDiscriminator().get());
         if (messageAuthor.getConnectedVoiceChannel().isPresent()) {
-            hover = hover.concat(" \n ");
+            hover = hover.concat("\n");
             hover = hover.concat("§6§lVocal§r: " + messageAuthor.getConnectedVoiceChannel().get().getName());
         }
-        hover = hover.concat(" \n ");
+        hover = hover.concat("\n");
         hover = hover.concat("§6§lRôle§r: " + server.getHighestRole(messageAuthor.asUser().get()).get().getName());
-        hover = hover.concat(" \n ");
+        hover = hover.concat("\n");
         Timestamp timestamp = new Timestamp(messageAuthor.getCreationTimestamp().toEpochMilli());
         Date date = new Date(timestamp.getTime());
         Timestamp timestamp2 = new Timestamp(server.getJoinedAtTimestamp(messageAuthor.asUser().get()).get().toEpochMilli());
         Date date2 = new Date(timestamp2.getTime());
         hover = hover.concat("§6§lRejoins Discord le§r: " + DateFormated(date));
-        hover = hover.concat(" \n ");
+        hover = hover.concat("\n");
         hover = hover.concat("§6§lRejoins serveur le§r: " + DateFormated(date2));
-        hover = hover.concat(" \n ");
+        hover = hover.concat("\n");
         ComponentBuilder hoverText = new ComponentBuilder(hover);
         PlayerComponent.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hoverText.create()));
         return PlayerComponent;
